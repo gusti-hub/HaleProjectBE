5e5f6d79781d9356f3e8660b641282d192965e94
Arkaprava23 <arkaprava.digitalexpeditor@gmail.com>
2024-07-08 11:47:11 +0530

    test

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.post('/api/signin', async (req, res) => {
     const { email, password } = req.body;
 
     if (!email || !password) {
-        return res.status(400).json({ message: 'Please provide credentials.' });
+        return res.status(400).json({ message: 'Please provide email and password.' });
     }
 
     try {
