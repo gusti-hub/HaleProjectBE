66197dd4cdda922ad857536f0897c20f7a5c44cc
Arkaprava23 <arkaprava.digitalexpeditor@gmail.com>
2024-07-08 11:38:45 +0530

    login-signup

--- index.js
+++ index.js
@@ -1,18 +1,85 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const connectDB = require('./config/db');
 require('dotenv').config();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const User = require('./models/User.js');
+
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+connectDB();
+
 app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
-const PORT = process.env.PORT || 5000;
+//Sign Up
+app.post('/api/signup', async (req, res) => {
+    const { name, email, password } = req.body;
+
+    console.log(name);
+
+    if (!name || !email || !password) {
+        return res.status(400).json({ message: 'Please provide name, email, and password' });
+    }
+
+    try {
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({ name, email, password: hashedPassword });
+        await newUser.save();
+
+        res.status(201).json({ message: 'User signed up successfully', user: newUser });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error });
+    }
+});
+
+//log in
+// Sign In
+app.post('/api/signin', async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ message: 'Please provide email and password' });
+    }
+
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: 'Invalid email or password' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Invalid email or password' });
+        }
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+            expiresIn: '1h',
+        });
+
+        res.status(200).json({ message: 'User signed in successfully', token });
+    } catch (error) {
+        console.error('Server error:', error); // Log the error
+        res.status(500).json({ message: 'Server error', error });
+    }
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
