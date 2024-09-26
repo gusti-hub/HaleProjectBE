3821fa2d6640cde0a397ae7b9abe6214526330d6
Arkaprava23 <arkaprava.digitalexpeditor@gmail.com>
2024-09-27 01:19:28 +0530

    expenses

---
+++ models/Expenses.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const expensesSchema = new mongoose.Schema({
+    prj: { type: String, required: true },
+    frmDate: { type: String, required: true },
+    toDate: { type: String, required: true },
+    type: { type: String, required: true },
+    amount: { type: Number, required: true },
+    totalAmount: { type: Number, required: true },
+    comment: { type: String, required: false, default: '' },
+    imageUrl: { type: String, required: false }
+}, { timestamps: true });
+
+const Expenses = mongoose.model('Expenses', expensesSchema);
+
+module.exports = Expenses;
--- routes/timeExpensesRoute.js
+++ routes/timeExpensesRoute.js
@@ -1,7 +1,36 @@
 const express = require('express');
 const auth = require('../utils/jwtUtils.js');
 const TimeExpenses = require('../models/Time&Expenses.js');
+const Expenses = require('../models/Expenses.js');
 
 const router = express.Router();
+
+router.post('/add-expense', async (req, res) => {
+    const { prj, frmDate, toDate, type, amount, totalAmount, comment, imageUrl } = req.body;
+
+    if (!prj || !frmDate || !toDate || !type || !amount || !totalAmount) {
+        return res.status(400).json({ message: 'Please provide all the mandatory details.' });
+    }
+
+    try {
+        const newExpense = new Expenses({ prj, frmDate, toDate, type, amount, totalAmount, comment, imageUrl });
+
+        await newExpense.save();
+
+        res.status(201).json({ message: 'Expense added successfully' });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error });
+    }
+});
+
+router.get('/expenses', auth, async (req, res) => {
+    try {
+        const expenses = await Expenses.find();
+        res.status(200).json(expenses);
+    } catch (error) {
+        console.error('Server error:', error);
+        res.status(500).json({ message: 'Server error', error });
+    }
+});
 
 module.exports = router;
